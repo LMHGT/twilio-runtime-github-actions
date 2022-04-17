3639450522ff877bb4b27d25bc87928b8e5b6d80
LMHGT <103891733+LMHGT@users.noreply.github.com>
2022-04-17 14:24:16 -0400

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello Twilio Function"
+        message : "Hello Twilio Function Call"
     }
 
     callback(null, result);
